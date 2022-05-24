c472ff8c22cecade3454208564849917e511dd56
amirxdbx <33214528+amirxdbx@users.noreply.github.com>
2022-05-24 04:13:27 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from pickle import load
-from bokeh.plotting import figure, show
 ################################################################
 
 st.write("""

fa83aa8d39366313f215017ea11ed15d13b58524
amirxdbx <33214528+amirxdbx@users.noreply.github.com>
2022-05-24 19:57:52 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import pickle
-@st.cache(hash_funcs={"MyUnhashableClass": lambda _: None}
+@st.cache(hash_funcs={builtins.dict: my_hash_func})
           
 def load_model():
     XGB=pickle.load(open('XGboost.pkl','rb'))
     RF=pickle.load(open('Random Forest.pkl','rb'))
     GBoost=pickle.load(open('Gradient Bossting.pkl','rb'))
     return XGB,RF,GBoost
+
 XGB,RF,GBoost=load_model()
 ################################################################
 

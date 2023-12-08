41cc4e59492708fa547d9ff8950ce8aa6047765a
amirxdbx <33214528+amirxdbx@users.noreply.github.com>
2023-12-08 12:18:45 +0000

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -13,15 +13,18 @@ def load_model_():
 
 tuned_model_ = load_model_()
 
-def cot(x): 
-    return 1/np.tan(x)
+# def cot(x): 
+#     return 1/np.tan(x)
 
-def unscalery(value,Label,NewDataset):
-    return np.exp(((value-0.1)*3.667910806940382)-7.900217131033494)
+# def unscalery(value):
+#     return np.exp(((value-0.1)*3.667910806940382)-7.900217131033494)
 
 # def calculate(values):    
 #     st.write(values)
-#     SFI = float(values.Af)/float(values.sf)+(float(values.Es)/float(values.Ef))*float(values.As)/float(values.ss)
+#     A_fpl = float(values.Af)/float(values.sf)
+#     A_spl= float(values.As)/float(values.ss)
+#     b_fl_b_w= float(values.b_fl)/float(values.b_w)
+    
 #     if values.Config [0]== 'Fully-wrapped':
 #         S_U_O = 0
 #     elif values.Config [0]== 'U-wrapped':
@@ -29,52 +32,52 @@ def unscalery(value,Label,NewDataset):
 #     else: 
 #         S_U_O = 2
 
-#     dataSample=pd.DataFrame(data={'SFI_factor':[SFI],'alpha':[float(values.alpha)],
-#                                   'w_s':[float(values.wf_sf)],'hf':[float(values.hf)],'S_U_O':[S_U_O],
-#                                   'fcm':[float(values.fcm)]})
+    # dataSample=pd.DataFrame(data={'A_fpl':[A_fpl],'A_spl':[A_spl],'alpha':[float(values.alpha)],
+    #                               'w_s':[float(values.wf_sf)],'hf':[float(values.hf)],'S_U_O':[S_U_O],
+    #                               'E_f':[float(values.E_f)]})
     
-#     e_fe = unscalery((predict_model(tuned_model_,dataSample).prediction_label),'e_fe',Main_data)[0]
-#     result = e_fe*float(values.get('Ef'))*float(values.get('Af'))/float(values.get('sf'))*np.sin(np.radians(dataSample.alpha))*dataSample.hf*(1+cot(np.radians(dataSample.alpha)))*np.sin(np.radians(dataSample.alpha))
+    # e_fe = unscalery((predict_model(tuned_model_,dataSample).prediction_label))[0]
+    # result = e_fe*float(values.get('Ef'))*float(values.get('A_fpl'))*np.sin(np.radians(dataSample.alpha))*dataSample.hf*(1+cot(np.radians(dataSample.alpha)))*np.sin(np.radians(dataSample.alpha))
 #     return result
 
-# st.write('Enter your beam data:')
-# col1, col2, col3 = st.columns([2,2,3])
+st.write('Enter your beam data:')
+col1, col2, col3 = st.columns([2,2,3])
 
-# with col1:
-#     Af= st.number_input("Area of FRP (mm2):", value=42)
-#     sf= st.number_input("sf (mm):", value=114)
-#     Es= st.number_input("Elasticity modulus of Steel (GPa):", value=200)
-#     Ef= st.number_input("Elasticity modulus of FRP (GPa):", value=218.4)
-#     As= st.number_input("Area of stirrups (mm2):", value=56.5)
-#     ss= st.number_input("ss (mm):", value=300)
-# with col2:
+with col1:
+    Af= st.number_input("Area of FRP (mm2):", value=42)
+    sf= st.number_input("sf (mm):", value=114)
+    Es= st.number_input("Elasticity modulus of Steel (GPa):", value=200)
+    Ef= st.number_input("Elasticity modulus of FRP (GPa):", value=218.4)
+    As= st.number_input("Area of stirrups (mm2):", value=56.5)
+    ss= st.number_input("ss (mm):", value=300)
+with col2:
     
-#     alpha= st.number_input("FRP orientation:", value=90)
-#     wf_sf= st.number_input("width to spacing ratio:", value=0.53)
-#     hf= st.number_input("Height of FRP reinforcement (mm):", value=300)
-#     fcm= st.number_input("Concrete compressive strength (MPa)", value=39.7)
-#     Config= st.radio('',options=("Fully-wrapped","U-wrapped","Side-bonded"),index=1) 
+    alpha= st.number_input("FRP orientation:", value=90)
+    wf_sf= st.number_input("width to spacing ratio:", value=0.53)
+    hf= st.number_input("Height of FRP reinforcement (mm):", value=300)
+    fcm= st.number_input("Concrete compressive strength (MPa)", value=39.7)
+    Config= st.radio('',options=("Fully-wrapped","U-wrapped","Side-bonded"),index=1) 
 
     
-# values=pd.DataFrame({
-#     'Af': [Af],
-#     'sf': [sf],
-#     'Es': [Es],
-#     'Ef': [Ef],
-#     'As': [As],
-#     'ss': [ss],
-#     'alpha': [alpha],
-#     'wf_sf': [wf_sf],
-#     'hf': [hf],
-#     'fcm': [fcm],
-#     'Config': [Config],
-# })
+values=pd.DataFrame({
+    'Af': [Af],
+    'sf': [sf],
+    'Es': [Es],
+    'Ef': [Ef],
+    'As': [As],
+    'ss': [ss],
+    'alpha': [alpha],
+    'wf_sf': [wf_sf],
+    'hf': [hf],
+    'fcm': [fcm],
+    'Config': [Config],
+})
 
-# with col3:
-#     st.empty()
-#     st.empty()
-#     st.empty()
-#     st.button('Calculate', key='Calculate')
+with col3:
+    st.empty()
+    st.empty()
+    st.empty()
+    st.button('Calculate', key='Calculate')
 #     out = st.empty()
 #     if st.session_state.get('Calculate'):
 #         result = np.round(calculate(values), 2)

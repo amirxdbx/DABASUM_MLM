f55d8fe9564f456dbffe6ff32fedca7fc85ac0c3
amirxdbx <33214528+amirxdbx@users.noreply.github.com>
2024-02-27 19:04:56 +0000

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -15,7 +15,7 @@ def resize_image(image, max_size=(600, 400)):
 image = Image.open('cross_section-ML.png')
 resized_image = resize_image(image, max_size=(600, 400))
 
-# st.set_page_config(page_title="FRP contribution to Shear resistance", page_icon=":guardsman:", layout="wide")
+st.set_page_config(page_title="FRP contribution to Shear resistance", page_icon=":guardsman:", layout="wide")
 
 # @st.cache_resource
 # def load_model_():

9a1749b1370cdeac79f7dc73bc757d35288cd54d
amirxdbx <33214528+amirxdbx@users.noreply.github.com>
2024-02-27 19:32:52 +0000

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -21,7 +21,7 @@ def load_model_():
     model = load_model('synth_xgboost')
     return model
 
-# tuned_model_ = load_model_()
+tuned_model_ = load_model_()
 
 def cot(x): 
     return 1/np.tan(x)

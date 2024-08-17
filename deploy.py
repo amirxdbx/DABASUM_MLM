c90ca333582f4d40babe32647b9c8d41e10bcb37
Amirhosein Mohammadi <33214528+amirxdbx@users.noreply.github.com>
2024-08-17 16:02:45 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -143,7 +143,6 @@ st.session_state.user_values = {
 with col5:
     st.button('Calculate', key='Calculate')
     out = st.empty()
-    # if st.session_state.get('Calculate'):
-    #     result = calculate(st.session_state.user_values)
-    #     out.text(f"Contribution of FRP to shear resistance: \n {result:.2f} kN")
-    st.session_state.user_values['fcm']
+    if st.session_state.get('Calculate'):
+        result = calculate(st.session_state.user_values)
+        out.text(f"Contribution of FRP to shear resistance: \n {result:.2f} kN")

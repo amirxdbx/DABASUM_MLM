5bd79525f17b5ef824aa3544213124085b762445
Amirhosein Mohammadi <33214528+amirxdbx@users.noreply.github.com>
2024-08-17 16:58:34 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -108,7 +108,7 @@ with col2:
     alpha_options = [45, 90]
     alpha = st.selectbox("Fibres orientation:", options=alpha_options, index=alpha_options.index(session_values['alpha']))
     config_options = ['Fully wrapped', 'U-wrapped', 'Side-bonded']
-    S_U_O = st.selectbox("FRP configuration:", options=config_options, index=config_options.index(session_values['S_U_O']))
+    S_U_O = st.selectbox("FRP configuration:", options=config_options, index=config_options.index(['U-wrapped']))
 
 with col3:
     b_fl = st.number_input("Width of beam flange (mm):", value=session_values['b_fl'])

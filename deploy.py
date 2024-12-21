6e131d525a0ef2bde628d30c5ef01fa395022a94
Amirhosein Mohammadi <33214528+amirxdbx@users.noreply.github.com>
2024-12-21 15:06:46 +0000

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -20,16 +20,15 @@ st.markdown(
         In this online app you can predict the contribution of FRP reinforcements to shear resistance of RC beams effortlessly and only providing the most relevant input parameters. 
         For further information, please refer to the paper at this link. 
         <br />
-        <a href="https://shear-frp-dabasum.streamlit.app/"><strong>Explore the App »</strong></a>
+        <a href="https://doi.org/10.1016/j.compstruct.2024.118632"><strong>Explore the App »</strong></a>
         <br />
-        <br />
-        <a href="https://shear-frp-dabasum.streamlit.app/">View Demo</a>
-        <a href="mailto:amir.xdbx@gmail.com">Report problems!</a>
+
       </p>
     </div>
     """,
     unsafe_allow_html=True
 )
+
 # Function to resize image
 def resize_image(image, max_size=(600, 400)):
     original_size = image.size
